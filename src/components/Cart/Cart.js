a6a49b9ddfdd7fdf76b0389d0e61e6eec587db5e
AbdulMobin <abdulmobin417@gmail.com>
2022-12-08 10:14:14 +0600

    Local storage use

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import './Cart.css'
+import React, { useState } from "react";
+import "./Cart.css";
 
 const Cart = (props) => {
-    const { cart } = props;
-    return (
-      <div>
-        <h3>Order Summary</h3>
-        <p>Selected Items: {cart.length}</p>
-        <p>Total Price: ${}</p>
-        <p>Total Shipping Charge: $5</p>
-        <p>Tax: $114</p>
-        <p>Grand Total: ${}</p>
-      </div>
-    );
+  const { cart } = props;
+  // console.log(cart);
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
+    shipping = shipping + product.shipping;
+  }
+
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
+
+  return (
+    <div className="cart">
+      <h3 className="heading">Order Summary</h3>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${shipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+    </div>
+  );
 };
 
 export default Cart;
+/*****************************************************
+                using reducer calculation
+*****************************************************/
+
+/******************Calculate quantity*******************
+const quantityReducer = (previous, current) => previous + current.quantity;
+const totalQuantity = cart.reduce(quantityReducer, 0);
+
+*****************Calculate total price*************
+const totalPriceReducer = (previous, current) => previous + current.price;
+const totalPrice = cart.reduce(totalPriceReducer, 0);
+
+*************calculate total shipping charge*********
+const totalShippingReducer = (prev, curr) => prev + curr.shipping;
+const totalShippingPrice = cart.reduce(totalShippingReducer, 0);
+
+const tax = parseFloat((totalPrice * 0.1).toFixed(2));
+const grandTotal = totalPrice + totalShippingPrice + tax;
+
+*********************************************************/
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -13,9 +14,34 @@ const Shop = () => {
       .then((data) => setproducts(data));
   }, []);
 
-  const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+  useEffect(() => {
+    console.log("local storage ", products);
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+
+  const handleAddToCart = (SelectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === SelectedProduct.id);
+    if (!exists) {
+      SelectedProduct.quantity = 1;
+      newCart = [...cart, SelectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== SelectedProduct.id);
+      SelectedProduct.quantity = SelectedProduct.quantity + 1;
+      newCart = [...rest, SelectedProduct];
+    }
     setCart(newCart);
+    addToDb(SelectedProduct.id);
   };
 
   return (
